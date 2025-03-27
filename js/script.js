74fd61acbcad1c998216d2ac6c2e1ef8af77371a
Ricardo-MaGuz <maguz.ricardo@gmail.com>
2025-03-27 10:34:38 -0600

    Added Click events for work places and projects

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function navigateOnClick(elementClass, url) {
+    const elements = document.getElementsByClassName(elementClass);
+    if (elements.length > 0) {
+        Array.from(elements).forEach((element) => {
+            element.addEventListener('click', () => {
+                window.open(url, '_blank');
+            });
+        });
+    } else {
+        console.error(`Elements with class "${elementClass}" not found.`);
+    }
+}
+
+navigateOnClick('sales-hub', 'https://sales-hub.com/');
+navigateOnClick('kavak-warranties', 'https://www.kavak.com/mx/lp/cuida-tu-auto/');
+navigateOnClick('kavak', 'https://www.kavak.com/mx/');
+navigateOnClick('widestudio', 'https://www.linkedin.com/company/widestudio-mx/about/');
+navigateOnClick('macmillan', 'https://www.macmillanprofesional.com.mx/');
+navigateOnClick('ixi', 'https://ixi.studio/');
+navigateOnClick('loto-audio', 'https://lotoaudio.com.mx/index.html');
+navigateOnClick('zombie', 'https://ricardo-maguz.github.io/I-AM-ZOMBIE/');
+navigateOnClick('dental-macplan', 'https://www.dentalmacplan.com/');
